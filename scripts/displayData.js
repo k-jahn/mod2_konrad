f554b196fc805dd69e1415e6ee42e9200c59a7f5
kj <konrad.jahn.84@gmail.com>
2017-12-05 11:42:20 +0100

    task3 functionality done... hopefully

--- scripts/displayData.js
+++ scripts/displayData.js
@@ -17,7 +17,11 @@ function DataHandler() {
         $('.memberTable').each(function() {
             that.displayMembers($(this))
         })
+        $('.statsTable').each(function() {
+            that.displayStats($(this))
+        })
     }
+
 
     // display function to build the states dropdown
     this.displayStates = function() {
@@ -30,14 +34,44 @@ function DataHandler() {
         $('#filterStates').append(optionArr)
     }
 
+    // diplay function to build stats table
+    this.displayStats=function(table){
+        var that=this;
+        table.find('.statsField').each(function(){
+            if ($(this).data("key")) {
+                var sum=0
+                var n=0
+                for (x of that.members){
+                    if (x.show('party')==$(this).data("party")){
+                        sum+= +x.show($(this).data("key"))
+                        n++
+                    }
+                }
+                if (n==0) $(this).html('--')
+                else $(this).html((sum/n).toFixed(2) + '%')
+            } else {
+                //display # of members
+                var n=0
+                for (x of that.members) {
+                    if (x.show('party')==$(this).data("party")) n++
+                }
+                $(this).html(n)
+            }
+
+        })
+
+    }
+
     // display function to build the table
     this.displayMembers = function(table) {
         //empty array for built rows
         var rowArr = [];
         //filter members who should appear in table
+        console.log('filtering ' +this.members.length)
         var tableMembers = this.filterMembers(this.members,table)
+        console.log('returning ' +tableMembers.length)
         //sort members
-        var tableMembers = this.sortMembers(tableMembers)
+        var tableMembers = this.sortMembers(tableMembers,table)
         // get data columns
         var columns = table.find('th')
         //loop to build individual rows and push them to rowArr
@@ -60,32 +94,64 @@ function DataHandler() {
 
     // filter members array before display in table
     this.filterMembers = function(members,table) {
-        //get values from inputs, if present
+        //filter from inputs
         if (table.data('filter')=='userInput'){
             var party = {
                 'D': $('#filterD').is(":checked"),
                 'R': $('#filterR').is(":checked"),
                 'I': $('#filterI').is(":checked")
             }
-            var state = $('#filterStates')[0].value || false;
+            var state = $('#filterStates')[0].value;
             return members.filter((member) => party[member.show('party')])
                 .filter((member) => !state || state == member.show('state'))
         }
+        //custom top/bot loy/attend filters
+        //crawler function to find  10% with stupid arbitrary criteria
+        function crawl(list,key){
+            var n=Math.round(list.length*0.1)-1
+            while(list[n+1].show(key)==list[n].show(key))n++
+            return list.slice(0,n)
+        }
+
+        if (table.data('filter')=='arbitraryLoyalty'){
+            var sortedMembers=this.sortMembers(members.filter((x)=>x.show('total_votes')!=0),table)
+            return crawl(sortedMembers,"votes_with_party_pct")
+        }
+        if (table.data('filter')=='arbitraryAttendance'){
+            var sortedMembers=this.sortMembers(members.filter((x)=>x.show('total_votes')!=0),table)
+            return crawl(sortedMembers,"missed_votes_pct")
+        }
+
+
+
         return members
     }
 
     // sort members array before display in table
-    this.sortMembers = function(members) {
-        return members
+    this.sortMembers = function(members,table) {
+        if (members.length==0) return [];
+        var sortKey=table.data('sortkey') || false;
+        var sortNeg=table.data('sortneg') || false;
+        var sortedMembers=members
+        console.log('sort function on, sorting ' +sortKey+' as '+typeof members[0].show(sortKey))
+        
+        if (typeof members[0].show(sortKey)=='number'){
+            sortedMembers.sort((a,b)=>a.show(sortKey)-b.show(sortKey))
+        }
+        if (typeof members[0].show(sortKey)=='string'){
+            sortedMembers.sort((a,b)=>a.show(sortKey)<b.show(sortKey)?-1:1)
+        }
+        if (sortNeg) sortedMembers=sortedMembers.reverse()
+        return sortedMembers
     }
 
     // sets this.members as array of initialized Politicans
     this.formatMembers = function(members) {
+        
         for (var i in members) {
-            members[i] = new Politician(members[i]);
-            members[i].init();
+            this.members.push(new Politician(members[i]));
+            this.members[i].init();
         }
-        this.members = members
     }
 
     // fetch states.json and call displayStates
@@ -146,12 +212,15 @@ function Politician(data) {
         }
     }
 
-    // initialize politician, setting full_name
+    // initialize politician
     this.init = function() {
-        var name = [this.data.first_name]; // build full name
+        //set full_name
+        var name = [this.data.first_name];
         if (this.data.middle_name) name.push(this.data.middle_name);
         name.push(this.data.last_name);
-        this.data.full_name = name.join(' '); //set full name
+        this.data.full_name = name.join(' '); 
+        //set party_votes
+        this.data.party_votes=Math.round(this.data.total_votes*this.data.votes_with_party_pct/100);
     }
 }
 
